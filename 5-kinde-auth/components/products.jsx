1d56b8edca42fa3710561b7c94b21a92e6e35362
SankThomas <thomassankara74@gmail.com>
2025-01-17 12:54:08 +0300

    create products component

---
+++ 5-kinde-auth/components/products.jsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div className="container mx-auto px-6 py-10">
+      {products ? (
+        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          {products.map((item) => (
+            <article
+              key={item.id}
+              className="rounded-lg border border-neutral-800 p-4 transition hover:bg-neutral-800"
+            >
+              <Image
+                src={item.image}
+                width={800}
+                height={600}
+                alt={item.title}
+                className="mb-2 h-[250px] rounded-lg object-contain"
+              />
+              <h2 className="mb-2 text-xl font-bold">{item.title}</h2>
+              <p className="text-sm leading-6 text-neutral-600">
+                {item.description}
+              </p>
+            </article>
+          ))}
+        </div>
+      ) : (
+        <p className="mt-10 text-center text-sm text-neutral-600">
+          Loading products...
+        </p>
+      )}
+    </div>
+  );
+}
