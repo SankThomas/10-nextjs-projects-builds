51ba4b88351225383a4fd0388eaad0ec5a8b518c
SankThomas <thomassankara74@gmail.com>
2025-01-21 09:00:39 +0300

    create products component

--- 5-kinde-auth/components/products.jsx
+++ 5-kinde-auth/components/products.jsx
@@ -6,18 +6,18 @@ import { useEffect, useState } from "react";
 export default function Products() {
   const [products, setProducts] = useState([]);
 
-  async function getProducts() {
-    const res = await fetch("https://fakestoreapi.com/products");
-    const data = await res.json();
-    setProducts(data);
-  }
-
   useEffect(() => {
+    async function getProducts() {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      setProducts(data);
+    }
+
     getProducts();
   }, []);
 
   return (
-    <div className="container mx-auto px-6 py-10">
+    <div className="container mx-auto px-4 py-10">
       {products ? (
         <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
           {products.map((item) => (
@@ -27,12 +27,12 @@ export default function Products() {
             >
               <Image
                 src={item.image}
+                alt={item.title}
                 width={800}
                 height={600}
-                alt={item.title}
-                className="mb-2 h-[250px] rounded-lg object-contain"
+                className="mb-4 h-[250px] w-full object-contain"
               />
-              <h2 className="mb-2 text-xl font-bold">{item.title}</h2>
+              <h2 className="mb-2 text-lg font-bold">{item.title}</h2>
               <p className="text-sm leading-6 text-neutral-600">
                 {item.description}
               </p>
@@ -40,7 +40,7 @@ export default function Products() {
           ))}
         </div>
       ) : (
-        <p className="mt-10 text-center text-sm text-neutral-600">
+        <p className="mt-10 text-center text-neutral-600">
           Loading products...
         </p>
       )}
